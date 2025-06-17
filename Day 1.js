1f1130211ccee4ecef5771b91ecfc3e2570e695d
Janarthanavel K <129484927+Janarthanavelba@users.noreply.github.com>
2025-06-17 21:57:45 +0530

    Create Day 1.js

---
+++ Day 1.js
@@ -0,0 +1,17 @@
+// Problem 1  (To find greatest of two numbers)
+var a=10
+var b=20
+if(a>b) console.log("A is Greater")
+else console.log("B is Greater")
+
+// Problem 2 (To find smallest of two numbers)
+var a=10
+var b=20
+if(a>b) console.log("A is Smaller")
+else console.log("B is smaller")
+
+// Problem 3 (To find the number is positive or negative or Zero)
+var num=09
+if(num>0)  console.log("Number is Positive")
+else if(num<0) console.log("Number is Negative")
+else console.log("Number is Zero")
